83446a3721aa1b1809fe52c9cfc2c6941737768a
akash rattan <akashrattan43@Gmail.com>
2020-10-28 16:45:14 -0700

    fetch

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
-const fs = require('fs')
+// const fs = require('fs')
 
-fs.writeFileSync('hello.txt', 'Hello from another file system')
+// fs.writeFileSync('hello.txt', 'Hello from another file system')
 
-const hobbies = ['sports', 'cricket']
+// const hobbies = ['sports', 'cricket']
 
-console.log(hobbies.map(Hobby => ('Hobby: ' + Hobby)))
+// console.log(hobbies.map(Hobby => ('Hobby: ' + Hobby)))
 
-hobbies.push('tennis')
-console.log(hobbies)
+// hobbies.push('tennis')
+// console.log(hobbies)
 
-const copy = [...hobbies]
-console.log(copy)
+// const copy = [...hobbies]
+// console.log(copy)
 
-const person = {
-    name: 'ak',
-    age: 26
+// const person = {
+//     name: 'ak',
+//     age: 26
+// }
+
+// const printName = ({name}) => {
+//     console.log(name)
+// }
+
+// printName(person)
+
+const fetchData = () => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Done!')
+        }, 1500)
+    });
+   return promise;
 }
 
-const printName = ({name}) => {
-    console.log(name)
-}
+setTimeout(() => {
+    console.log('Timer is done!')
+    fetchData().then(text => {
+        console.log(text)
+        fetchData().then(text2 => {
+            console.log(text)
+        })
+    })
+}, 2000)
 
-
-printName(person)
-
+console.log('hello')
