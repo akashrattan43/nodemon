081a45c110af0b7ae1d4fae41b86d44983f380ad
akash rattan <akashrattan43@Gmail.com>
2020-10-27 22:01:36 -0700

    es6

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-const fs = require('fs')
+// const fs = require('fs')
 
-fs.writeFileSync('hello.txt', 'Hello from another file system')
+// fs.writeFileSync('hello.txt', 'Hello from another file system')
+
+const hobbies = ['sports', 'cricket']
+
+console.log(hobbies.map(Hobby => ('Hobby: ' + Hobby)))
