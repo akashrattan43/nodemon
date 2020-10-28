ed00753ec84acfe2caebbf4ffe31610784defe18
akash rattan <akashrattan43@Gmail.com>
2020-10-28 16:30:27 -0700

    destructring

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-// const fs = require('fs')
+const fs = require('fs')
 
-// fs.writeFileSync('hello.txt', 'Hello from another file system')
+fs.writeFileSync('hello.txt', 'Hello from another file system')
 
 const hobbies = ['sports', 'cricket']
 
 console.log(hobbies.map(Hobby => ('Hobby: ' + Hobby)))
+
+hobbies.push('tennis')
+console.log(hobbies)
+
+const copy = [...hobbies]
+console.log(copy)
+
+const person = {
+    name: 'ak',
+    age: 26
+}
+
+const printName = ({name}) => {
+    console.log(name)
+}
+
+
+printName(person)
+
